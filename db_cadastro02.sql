04b8afec10c75fa0c5cb252dbc5de09773746f7e
Ana Caroline <84164877+carolfurtsant@users.noreply.github.com>
2022-08-19 11:41:33 -0300

    Curso MySQL #05 - Curso em Vídeo

---
+++ db_cadastro02.sql
@@ -0,0 +1,20 @@
+drop database cadastro; -- apaga banco
+
+create database CADASTRO
+default character set utf8
+default collate utf8_general_ci;
+
+use CADASTRO;
+
+create table PESSOAS (
+ID int not null auto_increment,
+NOME varchar(30) not null, -- não pode ser nulo
+NASCIMENTO date,
+SEXO enum('M','F'),
+PESO decimal (5,2),
+ALTURA decimal (3,2),
+NACIONALIDADE varchar (20) default 'Brasil',
+primary key (id) -- chave primária
+)default charset = utf8;
+
+describe pessoas
